3a17a63ea812fb5c11527fb5ecfe9544f7991355
Mateo Abascal <mateo@Mateos-MacBook-Pro.local>
2019-09-15 07:57:34 -0400

    blur

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,10 @@
 
 from flask import Flask, request, render_template
 from werkzeug import secure_filename
+
+from imageai.Detection.Custom import CustomObjectDetection
+from PIL import Image, ImageFilter
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,7 +16,26 @@ def upload():
 	if request.method == 'POST':
 		f = request.files['file']
 		f.save(secure_filename(f.filename))
+		takeImage(f.filename)
 		return render_template('image.html', image=f.filename)
 	return 'boo'
 
 app.run(host='0.0.0.0', port='80', debug='TRUE')
+
+function takeImage(filename):
+	detector = CustomObjectDetection()
+	detector.setModelTypeAsYOLOv3()
+	detector.setModelPath("../images/models/detection_model-ex-059--loss-0006.886.h5")
+	detector.setJsonPath("../images/json/detection_config.json")
+	detector.loadModel()
+	detections = detector.detectObjectsFromImage(input_image=filename, output_image_path="static/"+filename)
+	image = Image.open(filename)
+	for detection in detections:
+	    print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
+	    blurImage(image, detection["box_points"])
+	return "success"
+
+function blurImage(img, box[]):
+	cropped_img = image.crop(box)
+	blurred_img = cropped_image.filter(ImageFilter.GaussianBlur(radius=20))
+	img.paste(blurred_img, box)
