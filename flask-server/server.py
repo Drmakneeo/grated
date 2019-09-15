3655022731e2cc7870fa45263045dd1530534996
Mateo Abascal <mateo@Mateos-MacBook-Pro.local>
2019-09-15 08:14:25 -0400

    lmao

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,9 +1,12 @@
 
+import os
 from flask import Flask, request, render_template
 from werkzeug import secure_filename
 
 from imageai.Detection.Custom import CustomObjectDetection
 from PIL import Image, ImageFilter
+
+app.configure["UPLOAD_PATH"] = "static"
 
 app = Flask(__name__)
 
@@ -15,20 +18,21 @@ def home():
 def upload():
 	if request.method == 'POST':
 		f = request.files['file']
-		f.save(secure_filename(f.filename))
+		filename = secure_filename(f.filename)
+		f.save(os.path.join(app.configure["UPLOAD_PATH"], filename))
 		detector = CustomObjectDetection()
 		detector.setModelTypeAsYOLOv3()
 		detector.setModelPath("../images/models/detection_model-ex-059--loss-0006.886.h5")
 		detector.setJsonPath("../images/json/detection_config.json")
 		detector.loadModel()
-		detections = detector.detectObjectsFromImage(input_image=f.filename, output_image_path="static/"+f.filename)
-		image = Image.open(f.filename)
+		detections = detector.detectObjectsFromImage(input_image="static/"+f.filename, output_image_path="static/trashfire-"+f.filename)
+		image = Image.open("static/"+f.filename)
 		for detection in detections:
 			print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
 			cropped_img = image.crop(detection["box_points"])
 			blurred_img = cropped_img.filter(ImageFilter.GaussianBlur(radius=20))
 			image.paste(blurred_img, detection["box_points"])
-		return render_template('image.html', image=f.filename)
+		return render_template('image.html', image="static/"+f.filename)
 	return 'boo'
 
 app.run(host='0.0.0.0', port='80', debug='TRUE')
